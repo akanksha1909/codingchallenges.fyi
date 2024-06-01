7a3fb83d5bcd06bd1820a1d30697c3284a41f4db
akanksha1909 <akanksha1909singh@gmail.com>
2024-06-01 10:50:12 +0530

    Adds comments

--- tail-f/server.js
+++ tail-f/server.js
@@ -25,15 +25,8 @@ server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-const wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+// exisiting http server, we want to upgrade to websocket connection
+const wsServer = new WebSocketServer({ httpServer: server });
 
 let connections = []
 
